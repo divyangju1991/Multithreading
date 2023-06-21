d9c27f803c20c1774dcfaaabab37c363c916e08f
divyangju1991 <51605929+divyangju1991@users.noreply.github.com>
2023-06-21 19:17:55 +0530

    Update by While Loop

--- CustomThread/src/basics/PrintNumbersOddEven.java
+++ CustomThread/src/basics/PrintNumbersOddEven.java
@@ -4,9 +4,11 @@ class Printer2 implements Runnable {
 	static int counter = 1;
 	static Object lock = new Object();
 	int reminder;
+	int maxNumber;
 	
-	public Printer2(int reminder){
+	public Printer2(int reminder, int maxNumber){
 		this.reminder = reminder;
+		this.maxNumber = maxNumber;
 	}
 	
 	public void printer(){
@@ -16,19 +18,19 @@ class Printer2 implements Runnable {
 	@Override
 	public void run(){
 		
-		synchronized(lock){
-			for(int i=0; i<5; i++){
-				if(counter%2 != reminder){
-					try {
-						lock.wait();	
-					} catch(InterruptedException ex){
+			while(counter<maxNumber){
+				synchronized(lock){
+					if(counter%2 != reminder){
+						try {
+							lock.wait();	
+						} catch(InterruptedException ex){
 						
+						}
 					}
+					printer();
+					lock.notifyAll();
 				}
-				printer();
-				lock.notifyAll();
 			}
-		}
 	}
 }
 
@@ -36,8 +38,8 @@ public class PrintNumbersOddEven {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Printer2 oddObject = new Printer2(1);
-		Printer2 evenObject = new Printer2(0);
+		Printer2 oddObject = new Printer2(1, 10);
+		Printer2 evenObject = new Printer2(0, 10);
 		Thread oddThread = new Thread(oddObject, "ODD Thread");
 		Thread evenThread = new Thread(evenObject, "Even Thread");
 	
